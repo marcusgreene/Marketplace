9b338bd89c3d18e90b33497ca5dac821483e1a70
Marcus Greene <marcusgreene@Marcuss-MacBook-Air.local>
2022-05-11 23:11:24 -0400

    fixed imports

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-
-
+#assigns flask app name
 app = Flask(__name__)
+#config for flask sql
 app.config['SECRET_KEY'] = "secretkey"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///listingdatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+#database created
 db = SQLAlchemy(app)
 
 login = LoginManager(app)
@@ -20,8 +21,9 @@ def not_found(e):
     return render_template("404.html")
 
 
+from routes import *
+from models import *
 
 
-import routes, models
-
-db.create_all()
+if __name__ == "__main__":
+    app.run()
